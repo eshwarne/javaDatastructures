a848fa00daf20d49b2c1e5310aec4c24091a9e96
eshwarne <northeasteditsofficial@gmail.com>
2018-12-16 15:53:12 +0530

    BST

---
+++ BinaryTree/src/binaryTree/Main.java
@@ -0,0 +1,10 @@
+package binaryTree;
+import binaryTree.BinarySearchTree;
+public class Main {
+	public static void main(String[] args) {
+		BinarySearchTree bst=new BinarySearchTree(10);
+		bst.insert(5);
+		bst.insert(10);
+		bst.insert(11);
+	}
+}
