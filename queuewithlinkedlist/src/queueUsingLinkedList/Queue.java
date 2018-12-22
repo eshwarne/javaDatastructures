d40fc18804e29b39ca40c0715720ec58dbb113d4
eshwarne <northeasteditsofficial@gmail.com>
2018-12-22 22:43:37 +0530

    Queue Using Linked List (Enqueue Handled)

--- queuewithlinkedlist/src/queueUsingLinkedList/Queue.java
+++ queuewithlinkedlist/src/queueUsingLinkedList/Queue.java
@@ -1,9 +1,37 @@
 package queueUsingLinkedList;
 
 public class Queue<T> {
+	//Inner Class
 	public static class QueueNode<T>{
 		private T value;
-		private QueueNode<T> head;
-		private QueueNode<T> tail;
+		private QueueNode<T> next;
+		public QueueNode(T value) {
+			this.value=value;
+		}
+	}
+	//Properties
+	private QueueNode<T> head;
+	private QueueNode<T> tail;
+	int len=0;
+	public Queue(T value) {
+		QueueNode<T> node=new QueueNode<T>(value);
+		this.head=node;
+		this.tail=node;
+		this.len++;
+	}
+	public void enqueue(T value) {
+		QueueNode<T> node=new QueueNode<T>(value);
+		if(this.head!=null) {
+			this.tail.next=node;
+			this.tail=node;
+		}
+		if(this.head==null) {
+			this.head=node;
+			this.tail=node;
+		}
+		len++;
+	}
+	public T dequeue() {
+		
 	}
 }
