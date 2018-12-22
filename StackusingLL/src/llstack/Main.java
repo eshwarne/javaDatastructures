9d267a7aa579ba59116c766bea29049fd67d8676
eshwarne <northeasteditsofficial@gmail.com>
2018-12-22 18:02:46 +0530

    Stack Using Linked List (Dyanamic Data Type)

--- StackusingLL/src/llstack/Main.java
+++ StackusingLL/src/llstack/Main.java
@@ -2,8 +2,16 @@ package llstack;
 import llstack.Stack;
 public class Main {
 	public static void main(String[] args) {
-		Stack<Integer> s=new Stack<Integer>(2);
-		s.push(10);
+		//Integer
+//		Stack<Integer> s=new Stack<Integer>(2);
+//		s.push(10);
+//		System.out.println(s.seek());
+//		System.out.println(s.pop());
+//		System.out.println(s.pop());
+//		System.out.println(s.pop());
+		
+		Stack<String> s=new Stack<String>("Eshwar");
+		s.push("NE");
 		System.out.println(s.seek());
 		System.out.println(s.pop());
 		System.out.println(s.pop());
